5ef9b8311347fd1b17c05d4b7ae17ae2c11da469
amandaalves <amandaalves1034@gmail.com>
2017-07-09 10:42:22 -0300

    adicionado src (html,css,ts) do cap 2 e 3

---
+++ angular-heroes/src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  selectedHero : Hero;
+  heroes = HEROES;
+
+  onSelect(hero: Hero) : void {
+      this.selectedHero = hero;
+  }
+}
+src/app/app.module.ts Código fonte Typescript da aplicação
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
